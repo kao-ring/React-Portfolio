2b5be7a438310ae01083901dfedcd6294996594e
kao.ring <kao.ring@yahoo.com>
2020-09-03 21:45:56 -0400

    change to http2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,28 +6,28 @@ const Footer = () => {
     <footer className="fixed-bottom p-1 text-center text-white">
       <small>Copyright &copy; Kaori Caplan </small>
       <a
-        href="http://www.linkedin.com/in/kaori-nakajima-64750072/"
+        href="https://www.linkedin.com/in/kaori-nakajima-64750072/"
         target="_blank"
         rel="noopener noreferrer"
       >
         <img src="img/icons/linkedin.png" alt="LinkedIn" className="icon" />
       </a>
       <a
-        href="http://github.com/kao-ring"
+        href="https://github.com/kao-ring"
         target="_blank"
         rel="noopener noreferrer"
       >
         <img src="img/icons/github.png" alt="GitHub" className="icon" />
       </a>
       <a
-        href="http://www.facebook.com/kaori.k.nakajima.7"
+        href="https://www.facebook.com/kaori.k.nakajima.7"
         target="_blank"
         rel="noopener noreferrer"
       >
         <img src="img/icons/facebook.png" alt="FaceBook" className="icon" />
       </a>
       <a
-        href="http://www.youtube.com/channel/UCwFNHvQxnZfsfUm8V9B98kw?view_as=subscriber"
+        href="https://www.youtube.com/channel/UCwFNHvQxnZfsfUm8V9B98kw?view_as=subscriber"
         target="_blank"
         rel="noopener noreferrer"
       >
